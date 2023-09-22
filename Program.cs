36fc5005288275af447e675b78c94624b018ed56
aajunwa <45833272+aajunwa@users.noreply.github.com>
2023-09-22 16:15:57 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+AVL tree = new AVL();
+tree.Add(5);
+tree.Add(3);
+tree.Add(7);
+//tree.Add(2);
+// tree.Delete(7);
+
+Console.WriteLine();
+tree.DisplayTree();
+      
+    
+
+    
